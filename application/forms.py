5bd3aaceef62a089f38e6e641db355811da3766c
tommarvin <tommarvin2002@gmail.com>
2021-03-19 14:31:49 +0000

    completed jobs section of application

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, BooleanField
+from wtforms import StringField, IntegerField, SubmitField, BooleanField, DateField
 from wtforms.fields.core import BooleanField
 from wtforms.validators import DataRequired
 from . import app 
@@ -24,20 +24,19 @@ class WorkerForm(FlaskForm):
     BrickLaying = BooleanField(label='Brick Laying: ')
     Submit = SubmitField(label='Add Worker')
 
-class EditWorkerForm(FlaskForm):
-    Id = IntegerField(label='ID:', validators=[DataRequired()])
-    FirstName = StringField(label='First Name: ', validators=[DataRequired()])
-    LastName = StringField(label='Last Name: ', validators=[DataRequired()])
-    Age = IntegerField(label='Age: ', validators=[DataRequired()])
-    PhoneNumber = StringField(label='Phone Number: ', validators=[DataRequired()])
-    PhoneNumber2 = StringField(label='2nd Phone Number (Optional): ')
-    Email = StringField(label='Email Address: ', validators=[DataRequired()])
-    Email2 = StringField(label='2nd Email Address (Optional): ')
-    Workers_id = IntegerField(label='Workers Contact Details ID:', validators=[DataRequired()])
+class AddJobForm(FlaskForm):
+    Address1 = StringField(label='Address Line 1: ', validators=[DataRequired()])
+    Address2 = StringField(label='Address Line 2: ', validators=[DataRequired()])
+    Address3 = StringField(label='Address Line 3: ', validators=[DataRequired()])
+    Postcode = StringField(label='Postcode: ', validators=[DataRequired()])
+    StartDate = StringField(label='Start Date: ', validators=[ DataRequired()])
     Mowing = BooleanField(label='Mowing: ')
     Hedging = BooleanField(label='Hedging: ')
     Fencing = BooleanField(label='Fencing: ')
     Patios = BooleanField(label='Patios: ')
     BrickLaying = BooleanField(label='Brick Laying: ')
-    Workers_id = IntegerField(label='Workers Capability ID:', validators=[DataRequired()])
-    Submit = SubmitField(label='Update')
+    WorkersReq = IntegerField(label='Number of Workers required: ', validators=[DataRequired()])
+    CustomerName = StringField(label='Customers Full Name: ', validators=[DataRequired()])
+    CustomerPhone = StringField(label='Customers Phone number: ', validators=[DataRequired()])
+    CustomerEmail = StringField(label='Customers Email Address: ', validators=[DataRequired()])
+    Submit = SubmitField(label='Add Job')
--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,8 @@
 from application import app
 from flask import render_template, request, redirect
-from application.models import Capab, Workers, ConD1
+from application.models import Capab, Workers, ConD1, Jobs, Address, Req, CstmDt
 from application import db
-from application.forms import WorkerForm
+from application.forms import AddJobForm, WorkerForm
 
 
 
@@ -108,3 +108,64 @@ def remove(id):
 @app.route('/My-Workers/View-ContactDetails/<id>', methods=['GET', 'POST'])
 def View_CD(id):
     return render_template('view-contact-details.html', id=id, Workers=Workers, ConD1=ConD1)
+
+
+
+
+
+
+@app.route('/My-Jobs', methods=['GET', 'POST'])
+def viewjobs():
+    b = []
+    for worker in Workers.query.all():
+        b.append(worker)
+    x = (len(b)+50)
+    
+    return render_template('myjobs.html', Jobs=Jobs, Req=Req, Address=Address, CstmDt=CstmDt, x=x)
+
+
+@app.route('/My-Jobs/View-Customer-ContactDetails/<id>', methods=['GET', 'POST'])
+def View_Custm_CD(id):
+    return render_template('viewcustomer.html', id=id, CstmDt=CstmDt)
+
+@app.route('/My-Jobs/View-Address/<id>', methods=['GET', 'POST'])
+def View_Address(id):
+    return render_template('viewaddress.html', id=id, Address=Address)
+
+
+@app.route('/Add-Jobs', methods=['POST', 'GET'])
+def Add_Job():
+    form = AddJobForm()
+
+    if request.method  == 'POST':
+        start=form.StartDate.data
+        numbworkers=form.WorkersReq.data
+
+        mowing=form.Mowing.data
+        hedging=form.Hedging.data
+        fencing=form.Fencing.data
+        patios=form.Patios.data
+        bricklaying=form.BrickLaying.data
+
+        address1=form.Address1.data
+        address2=form.Address2.data
+        address3=form.Address3.data 
+        postcode=form.Postcode.data
+
+        customername=form.CustomerName.data
+        customerphone=form.CustomerPhone.data
+        customeremail=form.CustomerEmail.data
+
+        if form.validate_on_submit:
+            job1 = Jobs(start=start, numbworkers=numbworkers)
+            db.session.add(job1)
+            db.session.commit()
+            job2 = Req(mowing=mowing, hedging=hedging, fencing=fencing, patios=patios, bricklaying=bricklaying, jobs_id=job1.id)
+            db.session.add(job2)
+            job3 = Address(address1=address1, address2=address2, address3=address3, postcode=postcode, jobs_id=job1.id)
+            db.session.add(job3)
+            job4 = CstmDt(customername=customername, customerphone=customerphone, customeremail=customeremail, jobs_id=job1.id)
+            db.session.add(job4) 
+            db.session.commit()
+            return redirect('/My-Jobs')
+    return render_template('addjob.html', form=form)
